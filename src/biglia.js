db5efe45f17f1318bc526b2962d140e7e93a0204
Fabio <fabio@studiogusto.com>
2021-03-07 13:08:39 +0100

    First commit 🎉

--- src/biglia.js
+++ src/biglia.js
@@ -10,7 +10,7 @@ export default function Biglia() {
   const $petals = useRef()
   const $shadow = useRef()
   const $material = useRef()
-  const [textureShadow, textureMap] = useTexture(["./shadow.png", "./texture.jpg"])
+  const [textureShadow, textureMap] = useTexture(["https://supahfunk.github.io/biglia/shadow.png", "https://supahfunk.github.io/biglia/texture.jpg"])
   let isAnimating = false
 
   const colors = {
